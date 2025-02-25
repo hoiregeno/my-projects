5fa079383b013f71254756053d83b512aee3119f
Geno Hoire <hoiregeno75@gmail.com>
2025-02-25 12:03:19 +1000

    Added overlay effect

--- Navigation Bar/script.js
+++ Navigation Bar/script.js
@@ -1,5 +1,7 @@
 const navbar = document.getElementById("navbar");
+const navLinks = document.querySelectorAll("#navbar a ");
 
 function toggleSidebar() {
     navbar.classList.toggle("show-sidebar");
 }
+
