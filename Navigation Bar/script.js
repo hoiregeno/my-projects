f11c93f4d696246ef49de42dc9679980de4b1ba3
Geno Hoire <hoiregeno75@gmail.com>
2025-02-25 12:40:40 +1000

    Added functionality to back to top button.

--- Navigation Bar/script.js
+++ Navigation Bar/script.js
@@ -1,5 +1,6 @@
 const navbar = document.getElementById("navbar");
 const navLinks = document.querySelectorAll("#navbar a ");
+const topBtn = document.getElementById("top-btn");
 
 function toggleSidebar() {
     navbar.classList.toggle("show-sidebar");
@@ -8,3 +9,16 @@ function toggleSidebar() {
 navLinks.forEach(link => {
     link.addEventListener("click", e => toggleSidebar());
 });
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        topBtn.style.display = "flex";
+    }
+    else {
+        topBtn.style.display = "none";
+    }
+}
+
+function backToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
