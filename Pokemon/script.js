067f101f1333c7eae612c6306abd58745c7b108e
Geno Hoire <hoiregeno75@gmail.com>
2025-03-02 15:33:03 +1000

    Updated script.js

--- Pokemon/script.js
+++ Pokemon/script.js
@@ -1,14 +1,13 @@
+
 async function fetchData() {
     const pokemonName = document.getElementById("pokemon-name");
-    const message = document.getElementById("message");
 
     if (pokemonName.value === "") {
-        message.textContent = "Please enter something!";
-        message.style.display = "block";
+        window.alert("Please enter something!");
     }
     else {
         try {
-            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.value}`);
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.value.toLowerCase()}`);
 
             if (!response.ok) {
                 throw new Error("Could not find pokemon!");
@@ -17,12 +16,15 @@ async function fetchData() {
             const data = await response.json();
             const pokemonSprite = data.sprites.front_default;
 
+            const message = document.getElementById("message");
+            message.textContent = (pokemonName.value).charAt(0).toUpperCase() + (pokemonName.value).slice(1);
+            message.style.display = "block";
+
             const pokemonImg = document.getElementById("pokemon-sprite");
             pokemonImg.src = pokemonSprite;
             pokemonImg.style.display = "block";
 
-            pokemonName.value = ""; // Reset user input.
-            message.style.display = "none"; // Hide the message display.
+            pokemonName.value = ""; // Reset user input. 
         }
         catch (error) {
             console.error(error);
