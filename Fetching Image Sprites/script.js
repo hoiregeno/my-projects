229d8db64351d089bd92c9e01fecd7abe57312bd
Geno Hoire <hoiregeno75@gmail.com>
2025-03-03 22:13:11 +1000

    Updated JS

--- Fetching Image Sprites/script.js
+++ Fetching Image Sprites/script.js
@@ -1,25 +1,34 @@
 const form = document.getElementById("input-box");
 
 async function fetchSprite() {
-    const pokemonName = document.getElementById("pokemon-name").value.trim();
 
     try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        const pokemonName = document.getElementById("pokemon-name");
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.value.trim().toLowerCase()}`);
 
         if (!response.ok) {
             throw new Error("Could not find pokemon.");
         }
 
         const data = await response.json();
-        const pokemonSprite = data.sprites.front_default;
 
+        // DISPLAY POKEMON NAME.
+        const pokemonNameDisplay = document.getElementById("pokemon-name-display");
+        pokemonNameDisplay.textContent = data.name;
+        pokemonNameDisplay.style.display = "block";
+        // DISPLAY POKEMON SPRITE.
+        const pokemonSprite = data.sprites.front_default;
         const imgElement = document.getElementById("pokemon-sprite")
         imgElement.src = pokemonSprite;
         imgElement.style.display = "block";
+
+        pokemonName.value = ""; // Reset user input.
     }
     catch (error) {
+
         console.error(error);
     }
+
 }
 
 form.addEventListener("submit", e => e.preventDefault());
