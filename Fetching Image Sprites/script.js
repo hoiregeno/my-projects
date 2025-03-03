17089e58334da0cfbf4ee186032a24640e3042c8
Geno Hoire <hoiregeno75@gmail.com>
2025-03-03 22:38:26 +1000

    Update JS

--- Fetching Image Sprites/script.js
+++ Fetching Image Sprites/script.js
@@ -1,9 +1,15 @@
 const form = document.getElementById("input-box");
 
 async function fetchSprite() {
+    const pokemonNameDisplay = document.getElementById("pokemon-name-display");
+    const pokemonName = document.getElementById("pokemon-name");
 
     try {
-        const pokemonName = document.getElementById("pokemon-name");
+        if (!pokemonName.value.trim()) {
+            alert("Please enter a Pokémon name.");
+            return;
+        }
+
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.value.trim().toLowerCase()}`);
 
         if (!response.ok) {
@@ -13,22 +19,23 @@ async function fetchSprite() {
         const data = await response.json();
 
         // DISPLAY POKEMON NAME.
-        const pokemonNameDisplay = document.getElementById("pokemon-name-display");
         pokemonNameDisplay.textContent = data.name;
         pokemonNameDisplay.style.display = "block";
+
         // DISPLAY POKEMON SPRITE.
         const pokemonSprite = data.sprites.front_default;
         const imgElement = document.getElementById("pokemon-sprite")
         imgElement.src = pokemonSprite;
+        imgElement.alt = `Sprite of ${data.name}`;
         imgElement.style.display = "block";
 
         pokemonName.value = ""; // Reset user input.
     }
     catch (error) {
-
         console.error(error);
     }
-
 }
 
-form.addEventListener("submit", e => e.preventDefault());
+form.addEventListener("submit", e => {
+    e.preventDefault();
+});
