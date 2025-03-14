83683704333d5564d85d31347f95baf7c8850d72
Geno Hoire <hoiregeno75@gmail.com>
2025-03-14 22:26:59 +1000

    Updated WeatherApp.jsx

--- React Projects/weather-app/src/WeatherApp.jsx
+++ React Projects/weather-app/src/WeatherApp.jsx
@@ -7,20 +7,61 @@ function WeatherApp() {
     const [weather, setWeather] = useState(null);
     const [error, setError] = useState("");
 
+    const apiKey = import.meta.env.VITE_API_KEY;
+
+    const getWeatherData = async (city) => {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+
+            if (!response.ok) {
+                throw new Error(`Could not locate ${city}.`);
+            }
+
+            return await response.json();
+        }
+        catch (error) {
+            throw error;
+        }
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!city) {
+            setError("Please enter a city.");
+            setWeather(null);
+            return;
+        }
+
+        try {
+            const data = await getWeatherData(city);
+            setWeather(data);
+            setError("");
+        }
+        catch (error) {
+            setError(error.message);
+            setWeather(null);
+        }
+    }
+
+    const getWeatherEmoji = (weatherId) => {
+
+    }
+
     return (
         <div className="container">
             <h1>Weather App</h1>
-            <form className="weather-form">
-                <input type="text" placeholder="Enter city" />
+            <form className="weather-form" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Enter city" value={city} onChange={event => setCity(event.target.value)} />
                 <button type="submit">Find</button>
             </form>
 
             <div className="card">
-                <h2 className="city-display">Paris</h2>
-                <p className="temp-display">28°C</p>
-                <p className="humidity-display">Humidity: 79%</p>
-                <p className="desc-display">Moderate Rain</p>
-                <p className="weather-emoji">🌧️</p>
+                <h2 className="city-display">{data.name}</h2>
+                <p className="temp-display"></p>
+                <p className="humidity-display"></p>
+                <p className="desc-display"></p>
+                <p className="weather-emoji"></p>
             </div>
         </div>
     )
