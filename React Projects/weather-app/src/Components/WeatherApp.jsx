eb4b2cf534fe90f3fb50aafc91130011653e6024
Geno Hoire <hoiregeno75@gmail.com>
2025-03-16 15:13:36 +1000

    Updated WeatherApp.jsx

--- React Projects/weather-app/src/Components/WeatherApp.jsx
+++ React Projects/weather-app/src/Components/WeatherApp.jsx
@@ -67,16 +67,17 @@ function WeatherApp() {
                     value={city}
                     onChange={event => {
                         setCity(event.target.value);
+                        setError("");
                     }} />
                 <button type="submit">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path></svg>
                 </button>
             </form>
 
             {error && <p className="error-display">{error}</p>}
 
             {weather && <div className="card">
-                <h2 className="city-display">{weather.name}</h2>
+                <h2 className="city-display">{weather.name}, {weather.sys.country}</h2>
                 <p className="temp-display">{Math.floor(weather.main.temp - 273.15)}°C</p>
                 <p className="humidity-display">Humidity: {weather.main.humidity}%</p>
                 <p className="desc-display">{weather.weather[0].description}</p>
